08e9fd7234c657dd65414de83d083dd60e4614ff
seamchiminh <chiminh096@gmail.com>
2024-05-28 20:41:35 +0700

    final fix

--- components/cardComponent.js
+++ components/cardComponent.js
@@ -3,7 +3,7 @@ export const cardComponent = (product) => {
             <div
             class="w-80 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             <a href="#">
-                <img class="p-8 rounded-t-lg" src={product.image || "https://www.svgrepo.com/show/508699/landscape-placeholder.svg"} alt="product image" />
+                <img class="p-8 rounded-t-lg" src=${product?.image || "https://www.svgrepo.com/show/508699/landscape-placeholder.svg"} alt="product image" />
             </a>
             <div class="px-5 pb-5">
                 <a href="#">
--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@ import { cardComponent } from "../components/cardComponent.js";
 import { products } from "../data/products.js";
 
 console.log(products)
-let readerArea = document.querySelector('#card-area');
+let readerArea = document.querySelector("#card-area");
 
 products.map((pro) => {
-    readerArea.innerHTML = cardComponent(pro);
+    readerArea.innerHTML += cardComponent(pro);
 });
